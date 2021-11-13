4927d92400b1a0ec7f7ed3e8e2de533ef04fa9a4
mucsacso03 <39996411+mucsacso03@users.noreply.github.com>
2021-11-13 10:05:10 +0100

    backend w/o token

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import numpy as np
 app = Flask(__name__)
 api = Api(app)
 
+#TODO: token majd Sanyi nem a bodyban
 
 class registration(Resource):
     def post(self, username, password):
@@ -19,33 +20,33 @@ class login(Resource):
         return token
 
 class download(Resource):
-    def get(self, token, filename):
+    def get(self, filename):
         return 0
 
 class upload(Resource):
-    def post(self, token, file):
+    def post(self, file):
         return 0
 
 class comment(Resource):
-    def post(self, token, filename, comment):
+    def post(self, filename, comment):
         return 0
 
 class search(Resource):
-    def get(self, token, all, filename):
+    def get(self, all, filename):
         return 0
 
 class delete(Resource):
-    def post(self, token, filename):
+    def post(self, filename):
         return 0
 
 
 api.add_resource(registration, "/registration/<string:username>/<string:password>")
 api.add_resource(login, "/login/<string:username>/<string:password>")
-api.add_resource(download, "/download/<int:token>/<string:filename>")
-api.add_resource(upload, "/upload/<int:token>/<object??:file>")
-api.add_resource(comment, "/comment/<int:token>/<string:filename>/<string:comment>")
-api.add_resource(search, "/search/<int:token>/<bool:all>/<string:filename>")
-api.add_resource(delete, "/delete/<int:token>/<string:filename>")
+api.add_resource(download, "/download/<string:filename>")
+api.add_resource(upload, "/upload/<string:file>")
+api.add_resource(comment, "/comment/<string:filename>/<string:comment>")
+api.add_resource(search, "/search/<bool:all>/<string:filename>")
+api.add_resource(delete, "/delete/<string:filename>")
 
 if __name__ == '__main__':
     app.run(debug=True)
