c32351ca67f224a1c691f729b720a84c6bc02ae3
András Gedeon <andris2198@gmail.com>
2021-11-21 15:27:51 +0100

    Logolás szebb formátumban és main -> app

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from flask_restful import Api
+
+from datab.shared import db
+from resources.comment import Comment
+from resources.delete import Delete
+from resources.download import Download
+from resources.login import Login
+from resources.registration import Registration
+from resources.search import Search
+from resources.upload import Upload
+from logging.config import dictConfig
+
+# Configure logging first
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {
+        'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default',
+            'level': 'DEBUG'
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'level': 'DEBUG',
+            'formatter': 'default',
+            'filename': 'log/api.log',
+            'mode': 'a'
+        },
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi', 'file']
+    }
+})
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# db = SQLAlchemy(app)
+db.app = app
+db.init_app(app)
+
+# engine = db.create_engine('sqlite:///census.sqlite')
+# connection = engine.connect()
+# metadata = db.MetaData()
+# census = db.Table('census', metadata, autoload=True, autoload_with=engine)
+
+# TODO: Kiszedni a végén a kommentet is
+# db.drop_all()
+db.create_all()
+
+# TODO: token majd Sanyi nem a bodyban
+
+api.add_resource(Registration, "/registration")
+api.add_resource(Login, "/login")
+api.add_resource(Download, "/download/<string:fileid>")
+api.add_resource(Upload, "/upload")
+api.add_resource(Comment, "/comment")
+api.add_resource(Search, "/search/<int:get_all>/<string:from_date>/<string:to_date>")
+api.add_resource(Delete, "/delete")
+
+if __name__ == '__main__':
+    app.run(debug=True)
