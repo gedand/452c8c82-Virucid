5a68f9c5268a050b9d2439c0094252c11de90153
András Gedeon <andris2198@gmail.com>
2021-11-21 15:31:52 +0100

    Regisztráció validálás is működik

--- resources/registration.py
+++ resources/registration.py
@@ -1,51 +1,45 @@
 from flask import request
 from flask_restful import Resource
-
+from marshmallow import Schema, fields, ValidationError
 from datab.database import User
 from datab.shared import db
-from methods.input_sanitization import input_sanitization
+from flask import current_app  as app
+from flask_api import status
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Hash import SHA512
+from Crypto.Random import get_random_bytes
 
+from validators.registration_validator import RegistrationValidator
+
+class RegistrationSchema(Schema):
+    validator = RegistrationValidator()
+    username = fields.Str(required=True,error_messages={"required": "Username is required."}, validate=validator.username_check)
+    password = fields.Str(required=True, error_messages={"required": "Password is required."}, validate=validator.password_check)
 
 class Registration(Resource):
+    def __init__(self):
+        super().__init__()
+        self.schema = RegistrationSchema()
+
     def post(self):
         try:
-            # input sanitization
-            if request.method == 'POST':
-                username = input_sanitization(request.form['username'])
-                password = input_sanitization(request.form['password'])
-            else:
-                # raise Exception("Not post", request.method)
-                return str(403)
-            # is_username_available
-            if self.is_username_available(username):
-                # is_username_available: ALT TRUE
-
-                # password_complexity_check
-                if self.password_complexity_check(password):
-                    # password_complexity_check ALT TRUE
-
-                    # send_registration_parameters
-                    guest = User(username=username, password=password, is_admin=0)
-                    db.session.add(guest)
-                    db.session.commit()
-                    # successful_registration_feedback
-                    return str(200)
-                else:
-                    # password_complexity_check ALT FALSE
-                    return str(401)
-            else:
-                # is_username_available: ALT FALSE
-                return str(403)
+            # Validates input
+            credentials = self.schema.load(request.form)
+            username = credentials['username']
+            password_hash, salt = self.hash_password(credentials['password'])
+            print(password_hash)
+            guest = User(username=username, password=password_hash, salt=salt, is_admin=0)
+            db.session.add(guest)
+            db.session.commit()
+            # successful_registration_feedback
+            return 'User successfully registered', status.HTTP_200_OK
+        except ValidationError as v:
+            app.logger.error(v)
+            return str(v), status.HTTP_400_BAD_REQUEST
         except Exception as e:
-            print(e)
-
-    def is_username_available(self, username):
-        u = User.query.filter_by(username=username).first()
-        if u:
-            return False
-        else:
-            return True
-
-    def password_complexity_check(self, password):
-        # TODO: implement
-        return True
+            app.logger.error(e)
+            return 'SERVER ERROR', status.HTTP_500_INTERNAL_SERVER_ERROR
+    
+    def hash_password(self, password):
+        salt = get_random_bytes(16)
+        return PBKDF2(password.encode('UTF-8'), salt, dkLen=16, count=200000, hmac_hash_module=SHA512), salt
