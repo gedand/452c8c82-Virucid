d3147e47f4201ed9031483aaeddc4595b1f472ec
Papp Benjamin <papp.benjamin.hun@gmail.com>
2021-11-29 00:13:27 +0100

    untested, done

--- web/index.js
+++ web/index.js
@@ -1,19 +1,24 @@
+
+var array1=[1,1,2];
+var extension=".jpg";
 function picturesShow(array)
 {
+  array1=array;
   var arr=["assets/images/work-2.jpg","assets/images/work-2.jpg","assets/images/work-2.jpg"];
 
     //var arr=['omae','mo','shindeiru'];
     var ul=document.getElementById('works-grid');
     removeAllChildNodes(ul);
+    var url="http://localhost:5000/download/";
     
 
-    for(i=0; i<=arr.length-1;i++)
+    for(i=1; i<=Object.keys(array.files).length-1;i++)
     {
         var li=document.createElement('li');
         li.className="work-item"
         //li.style.position='absolute';
         link = document.createElement('a');
-        link.setAttribute('href', 'single_image.html?imageId='+arr[i]);
+        link.setAttribute('href', 'single_image.html?imageId='+array.files[i]);
        /* link.onclick = function (this) { 
             console.log(this)
             return false;
@@ -23,7 +28,7 @@ function picturesShow(array)
         var workImageDiv=document.createElement('div');
         workImageDiv.className='work-image';
         var workImageImg=document.createElement('img');
-        workImageImg.src=arr[i];
+        workImageImg.src=url+array.files[i]+extension;
         //workImageDiv.appendChild(workImageImg);
         
         
@@ -51,8 +56,59 @@ function picturesShow(array)
     }
    
 }
-function picturesGet(date1,date2)
+
+function onloadContent()
 {
+  fileUpload1()
+  picturesGet();
+}
+
+function fileUpload1()
+{
+  var url = "http://localhost:5000/upload"
+  const input = document.querySelector('#avatars');
+
+// Listen for file selection event
+input.addEventListener('change', (e) => {
+    fileUpload(input.files[0]);
+});
+
+// Function that handles file upload using XHR
+const fileUpload = (file) => {
+    // Create FormData instance
+    const fd = new FormData();
+    fd.append('file', file);
+
+    // Create XHR rquest
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    var token=sessionStorage.getItem('token')
+    
+    xhr.setRequestHeader("Authorization", "Bearer "+token );
+    
+    // Log HTTP response
+    xhr.onload = () => {
+        console.log(xhr.response);
+    };
+
+    // Send XHR reqeust
+    
+
+    xhr.send(fd);
+};
+}
+function picturesGet()
+{
+
+
+
+
+
+
+
+
+
+
     var url = "http://localhost:5000/search";
 
     var xhr = new XMLHttpRequest();
@@ -65,7 +121,7 @@ function picturesGet(date1,date2)
         const responseObj = JSON.parse(xhr.responseText);
       if(xhr.status==200)
       {
-        picturesShow(responseObj)
+        picturesShow(responseObj);
       }
      
 
--- web/single_image.js
+++ web/single_image.js
@@ -1,5 +1,9 @@
+var extension=".jpg";
+var extensionCaff=".caff"
+var urlStart="http://localhost:5000/download/";
 function contentsLoad()
 {
+  
     var url=window.location.href
     let paramString = url.split('?')[1];
     let queryString = new URLSearchParams(paramString);
@@ -9,18 +13,20 @@ function contentsLoad()
     }
     var mainImage=document.getElementById('mainImage');
     var imageId=sessionStorage.getItem('imageId');
-    mainImage.src=imageId;
+    mainImage.src=urlStart+imageId+extension;
+
+    commentFetch();
 
 }
 function commentUploadPost()
 {
     var url = "http://localhost:5000/comment";
     var formData = new FormData();
-    var fileId=sessionStorage.getItem('imageId');
+    var filename=sessionStorage.getItem('imageId');
     comment=document.getElementById('commentInput').value;
 
     formData.append("comment", comment);
-    formData.append("fileId", fileId); 
+    formData.append("filename", filename); 
 
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url);
@@ -40,3 +46,135 @@ function commentUploadPost()
 
     xhr.send(formData);
 }
+
+function commentUploadPost1()
+{
+    var url = "http://localhost:5000/comment";
+    var formData = new FormData();
+    var fileId=sessionStorage.getItem('imageId');
+    comment=document.getElementById('commentInput').value;
+
+    formData.append("comment", comment);
+    formData.append("file_id", "xewd8pdp3tc53j86"); 
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    var token=sessionStorage.getItem('token')
+    
+    xhr.setRequestHeader("Authorization", "Bearer "+token );
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        const responseObj = JSON.parse(xhr.responseText);
+      if(xhr.status==200)
+      {
+        picturesShow(responseObj)
+      }
+     
+
+    }};
+
+    xhr.send(formData);
+}
+function commentFetch()
+{
+    var url = "http://localhost:5000/comment";
+    var formData = new FormData();
+    var filename=sessionStorage.getItem('imageId');
+
+    formData.append("filename", filename); 
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    var token=sessionStorage.getItem('token')
+    
+    xhr.setRequestHeader("Authorization", "Bearer "+token );
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        const responseObj = JSON.parse(xhr.responseText);
+      if(xhr.status==200)
+      {
+        generateComments(responseObj)
+      }
+     
+
+    }};
+
+    xhr.send(formData);
+}
+function generateComments(data)
+{
+  //data=["見えないとこでバタ足するんです","見えないとこでバタ足するんです","見えないとこでバタ足するんです"];
+  var div=document.getElementById('commentDiv');
+  for(i=0; i<=Object.keys(data.comments).length-1;i++)
+    {
+  var containerDiv=document.createElement('div');
+  containerDiv.className='mt-4 text-justify float-left';
+  var commentP=document.createElement('p')
+  var text =data.comments[i]
+  commentP.append(text);
+  containerDiv.appendChild(commentP);
+  div.appendChild(containerDiv);
+  
+  } 
+  
+
+
+
+}
+function deleteImagePost()
+{
+  var url = "http://localhost:5000/delete";
+  var formData = new FormData();
+  var fileId=sessionStorage.getItem('imageId');
+
+  formData.append("filename", fileId); 
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+  var token=sessionStorage.getItem('token')
+  
+  xhr.setRequestHeader("Authorization", "Bearer "+token );
+  xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+      const responseObj = JSON.parse(xhr.responseText);
+    if(xhr.status==200)
+    {
+      window.location.href = 'index.html';
+    }
+   
+
+  }};
+
+  xhr.send(formData);
+
+
+}
+function downloadImagePNG()
+{
+  var source=urlStart+sessionStorage.getItem('imageId')+extension;
+  const fileName = source.split('/').pop();
+	var el = document.createElement("a");
+	el.setAttribute("href", source);
+	el.setAttribute("download", fileName);
+	document.body.appendChild(el);
+ 	el.click();
+	el.remove();
+
+
+}
+function downloadImageCaff()
+{
+  
+  var source=urlStart+sessionStorage.getItem('imageId')+extensionCaff;
+  const fileName = source.split('/').pop();
+	var el = document.createElement("a");
+	el.setAttribute("href", source);
+	el.setAttribute("download", fileName);
+	document.body.appendChild(el);
+ 	el.click();
+	el.remove();
+
+
+}
+
+
